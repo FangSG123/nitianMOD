b883adb7a00eeb872d80b28f6aacf61f9f3d06c6
16609 <1660903022@qq.com>
2024-11-06 10:10:03 +0800

    无脑农场主

--- src/main/java/com/ntsw/entity/NongChangZhuEntity.java
+++ src/main/java/com/ntsw/entity/NongChangZhuEntity.java
@@ -1,89 +1,48 @@
 package com.ntsw.entity;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.memory.MemoryModuleType;
-import net.minecraft.world.entity.ai.memory.MemoryStatus;
-import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvents;
-import java.util.Optional;
 
 public class NongChangZhuEntity extends PathfinderMob {
 
     public NongChangZhuEntity(EntityType<? extends PathfinderMob> entityType, Level world) {
         super(entityType, world);
-    }
+        // 设置基础移动速度和其他属性
+        this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.5D);
+        this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(3.0D);
 
-    @Override
-    protected Brain.Provider<NongChangZhuEntity> brainProvider() {
-        return Brain.provider(createMemoryTypes(), ImmutableList.of());
-    }
-
-    private static ImmutableList<MemoryModuleType<?>> createMemoryTypes() {
-        return ImmutableList.of(
-                MemoryModuleType.LOOK_TARGET,
-                MemoryModuleType.WALK_TARGET,
-                MemoryModuleType.HURT_BY,
-                MemoryModuleType.NEAREST_VISIBLE_PLAYER,
-                MemoryModuleType.NEAREST_ATTACKABLE
-        );
+        // 设置手持物品为栓绳
+        this.setItemInHand(net.minecraft.world.InteractionHand.MAIN_HAND, new ItemStack(Items.LEAD));
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new RandomStrollGoal(this, 0.6D));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
-        this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, LivingEntity.class, 8.0F));
+        // 添加基础的村民行为
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, LivingEntity.class, 8.0F));
+        this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1.0D));
 
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, LaoHeiEntity.class, true));
-        this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
+        // 添加攻击目标，设为最近的 LaoHei 实体
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, LaoHeiEntity.class, true));
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0D, true));
     }
-
-    @Override
-    protected void customServerAiStep() {
-        this.level().getProfiler().push("nongchangzhuBrain");
-        this.getBrain().tick((ServerLevel) this.level(), this);
-        this.level().getProfiler().pop();
-    }
-
-    @Override
-    public Brain<NongChangZhuEntity> getBrain() {
-        return (Brain<NongChangZhuEntity>) super.getBrain();
-    }
-
-    @Override
-    protected void defineSynchedData() {
-        super.defineSynchedData();
-    }
-
-    @Override
-    public void addAdditionalSaveData(CompoundTag tag) {
-        super.addAdditionalSaveData(tag);
-    }
-
-    @Override
-    public void readAdditionalSaveData(CompoundTag tag) {
-        super.readAdditionalSaveData(tag);
-    }
-
     public static AttributeSupplier.Builder createAttributes() {
-        return PathfinderMob.createMobAttributes()
-                .add(Attributes.MOVEMENT_SPEED, 0.6D)
-                .add(Attributes.ATTACK_DAMAGE, 3.0D)
-                .add(Attributes.MAX_HEALTH, 20.0D);
+        return Monster.createMonsterAttributes()
+                .add(Attributes.MAX_HEALTH, 120.0)
+                .add(Attributes.MOVEMENT_SPEED, 0.2)
+                .add(Attributes.FLYING_SPEED, 0.4)
+                .add(Attributes.FOLLOW_RANGE, 30.0)
+                .add(Attributes.ATTACK_DAMAGE, 1.0)
+                .add(Attributes.ARMOR, 1.0);
     }
 }
